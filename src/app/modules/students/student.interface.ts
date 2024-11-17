a616e3f49b2a4046c27e78db2f3fec2bf5a44665
hmmasudsarder <hmmasudsarder1@gmail.com>
2024-11-17 12:56:20 +0600

    student interface finish

--- src/app/modules/students/student.interface.ts
+++ src/app/modules/students/student.interface.ts
@@ -1,15 +1,41 @@
 import { Schema, model, connect } from 'mongoose';
 
 // 1. Create an interface representing a document in MongoDB.
+export interface Guardian {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+}
+
+export interface UserName {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+}
+
+export interface LocalGuardian {
+  name: string;
+  occupation: string;
+  contactNO: string;
+  address: string;
+}
+
 export type Student = {
-    id: string;
-  name: {
-    firstName: string;
-    middleName: string;
-    lastName: string;
-  },
-  gender: "male"| "female";
+  id: string;
+  name: UserName;
+  gender: 'male' | 'female';
   dateOfBirth: string;
   email: string;
-  contactNo: number;
-}
+  contactNo: string;
+  emergencyContactNo: string;
+  bloogGroup: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  localGuardian: LocalGuardian;
+  profileImg: string;
+  isActive: 'active' | 'block';
+};
