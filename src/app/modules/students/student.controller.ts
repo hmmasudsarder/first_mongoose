0824f0a6245f836502cb08c0a04c8eab6b63f3d8
hmmasudsarder <hmmasudsardar@gmail.com>
2024-11-20 13:19:31 +0600

    student create successfully

--- src/app/modules/students/student.controller.ts
+++ src/app/modules/students/student.controller.ts
@@ -4,6 +4,7 @@ import { StudentService } from './student.service';
 const createStudent = async (req: Request, res: Response) => {
   try {
     const {student: studentData} = req.body;
+    console.log(studentData)
     const result = await StudentService.createStudentIntoDB(studentData);
     res.status(200).json({
       success: true,
